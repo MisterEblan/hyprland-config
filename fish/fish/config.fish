bdbfebf5eee309dcdbace041f8ad115da7529243
MisterEblan <boy4rov.da@gmail.com>
2025-06-01 22:27:33 +0300

    New nvim plugin added

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -2,4 +2,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fastfetch
 end
+alias vpn="sudo openvpn --config ~/openvpn_config/amnezia --daemon"
 set -gx PATH $HOME/.local/bin $PATH
+
+# pyenv config for fish
+set -x PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/bin $PATH
+
+# инициализация
+status --is-interactive; and pyenv init - | source
+status --is-interactive; and pyenv virtualenv-init - | source
--- nvim-conf/nvim/after/plugin/lsp.lua
+++ nvim-conf/nvim/after/plugin/lsp.lua
@@ -43,7 +43,7 @@ local cmp_select = {behaviour = cmp.SelectBehavior.Select}
 local cmp_mappings = cmp.mapping.preset.insert({
         ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Enter>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
 })
 
